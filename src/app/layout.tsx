ffe6d385c6716584da28a6df12b771ee49700aec
william <william.frazao91@gmail.com>
2025-09-18 11:34:59 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { type_second } from '../functions/fonts';
+
+export const metadata: Metadata = {
+  title: 'Dogs Next',
+  description: 'Rede social para cachorros.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={type_second.variable}>{children}</body>
+    </html>
+  );
+}
