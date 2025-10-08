c70c182786c81606fba4caddc9539c48a48cfeaa
william <william.frazao91@gmail.com>
2025-10-08 10:49:49 -0300

    Login useForm

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,12 +1,37 @@
-import login from '@/actions/login';
+'use client';
 
-export default async function LoginForm() {
+import login from '@/actions/login';
+import { useFormState, useFormStatus } from 'react-dom';
+import Button from '@/components/forms/button';
+
+function FormButton() {
+  const { pending } = useFormStatus();
+
   return (
     <>
-      <form action={login}>
+    {pending ? (
+      <Button disabled={pending}>Enviando...</Button>
+    ) : (
+      <Button>Entrar</Button>
+    )}
+    </>
+  );
+}
+
+export default function LoginForm() {
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: '',
+    data: null,
+  });
+
+  return (
+    <>
+      <form action={action}>
         <input type="text" name="username" placeholder="usuário" />
         <input type="password" name="password" placeholder="senha" />
-        <button>Entrar</button>
+        <FormButton />
+        <p>{state.error}</p>
       </form>
     </>
   );
