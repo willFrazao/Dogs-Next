5f7ba0135aa3e255afd3e17f9b66817186510526
william <william.frazao91@gmail.com>
2025-10-28 15:15:36 -0300

    create context

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,7 +1,14 @@
-export default async function ContaPage() {
+'use client';
+import { useUser } from '@/context/user-context';
+import React from 'react';
+
+
+export default function ContaPage() {
+  const { user } = useUser();
+  console.log(user);
   return (
     <main>
-      <h1>Conta</h1>
+      <h1>Conta: {user?.nome}</h1>
     </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,33 @@
 import type { Metadata } from 'next';
+// @ts-ignore: allow importing global CSS without a declaration file
 import './globals.css';
 import { type_second } from '../functions/fonts';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import { UserContextProvider } from '@/context/user-context';
+import userGet from '@/actions/user-get';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
   description: 'Rede social para cachorros.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data } = await userGet();
   return (
     <html lang="pt-BR">
       <body className={type_second.variable}>
-        <div className='App'>
-          <Header />
-          <main className='AppBody'>{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={data}>
+          <div className='App'>
+            <Header />
+            <main className='AppBody'>{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );
