cba5b840c93d83dc55898b588a85013815e75d19
william <william.frazao91@gmail.com>
2025-11-03 15:10:28 -0300

    Feed conta

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,13 +1,38 @@
+import photosGet from '@/actions/photos-get';
+import userGet from '@/actions/user-get';
+import Feed from '@/components/feed/feed';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Minha Conta',
 };
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const { data: user, error: userError } = await userGet();
+  
+  if (userError) {
+    return (
+      <main>
+        <p>{userError}</p>
+      </main>
+    );
+  }
+
+  const { data, error: photosError } = await photosGet({ user: user?.username });
+  
   return (
     <main>
-      <h1>Conta</h1>
+      {photosError ? (
+        <p>{photosError}</p>
+      ) : data && data.length > 0 ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+            <p style={{ color: '#444', fontSize: '1.25rem', marginBottom: '1rem' }}>Nenhuma foto encontrada.</p>
+          <Link href={'/conta/postar'} className='button' style={{ display: 'inline-block' }}>Postar Foto</Link>
+        </div>
+      )}
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@ import photosGet from '@/actions/photos-get';
 import Feed from '@/components/feed/feed';
 
 export default async function Home() {
-  const data = await photosGet();
+  const { data, error } = await photosGet();
   return (
     <section className="container main-container">
-      <Feed photos={data} />
+      {error ? (
+        <p>{error}</p>
+      ) : data ? (
+        <Feed photos={data} />
+      ) : (
+        <p>Nenhuma foto encontrada.</p>
+      )}
     </section>
   );
 }
